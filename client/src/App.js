ab0c8a289754d99397a0dff016b6568bc7d913cd
Baptiste BOUCHER <bboucher@e1r1p10.42.fr>
2019-08-03 12:00:57 +0200

    setup dropdown selection
    Signed-off-by: Baptiste BOUCHER <bboucher@e1r1p10.42.fr>

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Select from 'react-select';
 
 function App() {
   const [data, setData] = useState(null); 
+  const [selectedOption, setSelectedOption] = useState(null); 
+  const [options, setOptions] = useState(null); 
 
   useEffect(() => {
     axios.get('/inventory')
-      .then(res => { console.log(res); })
+      .then(res => { 
+        setData(res.data);
+        const productNames = res.data.map(elem => elem.product_name);
+        const productNamesUnique = productNames.filter((elem, index) => productNames.indexOf(elem) === index);
+        const formatedOptions = productNamesUnique.map(elem => {return { value: elem, label: elem }});
+        setOptions(formatedOptions);
+      })
       .catch(error => { console.log(error); })
   }, []);
 
+  const handleChange = selection => {
+    setSelectedOption(selection);
+    console.log(`Option selected:`, selection);
+  };
+
   return (
-    <p>Hello</p>
+    <Select
+      value={selectedOption}
+      onChange={handleChange}
+      options={options}
+    />
   );
 }
 
